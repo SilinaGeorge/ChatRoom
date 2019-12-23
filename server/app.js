628c4b126d441bd2ffcb574a58b5c243cb72c3c8
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-22 20:45:24 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+// imports
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const router = require('./router');
+
+const app = express();
+
+// create socket and server
+const server =http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket)=>{
+    console.log('new socket connection')
+
+    socket.on('disconnect', () =>{
+        console.log('socket has been disconnected')
+    })
+
+});
+
+
+app.use(router);
+const port = process.env.PORT || 5000;
+server.listen( port, ()=> console.log('listening on 3000'));
+
+
+
