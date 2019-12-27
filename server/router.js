76e0b84f357bb48eaf6939287323d8b12297e25a
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-27 16:56:16 -0500

    Update router.js

--- server/router.js
+++ server/router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 express.static(path.join(__dirname, '../client/public'))
 
 router.get('/', (req,res) => {
+    console.log(__dirname)
     res.sendFile("index.html", {root: '../'});
 });
 
