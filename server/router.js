358c7ea92e1e019b3dd0f7b0305f57dca6458c33
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-26 23:32:55 -0500

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+express.static(path.join(__dirname, '../client/public'))
 
 router.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, "../client", 'public','index.html'))
