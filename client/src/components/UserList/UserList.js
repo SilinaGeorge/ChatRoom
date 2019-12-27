cc661a7f2dd2f021af9a2fff271e01906b170e83
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-26 21:45:08 -0500

    format userlist.js

--- client/src/components/UserList/UserList.js
+++ client/src/components/UserList/UserList.js
@@ -1,28 +1,20 @@
 import React from 'react'
 
-const UserList = ({roomname, userList}) => (
+const UserList = ({ roomname, userList }) => (
    userList.users
-   ?
-       <div style={{ height:window.innerHeight - 120 + 'px', overflowY:'auto', wordWrap:'break-word' }}>
-          <h5 style={{textAlign:'center'}}>Users in {roomname}</h5>
-          <ul>
-         {
-            
-         userList.users.map((user, i) => (
-         <li key={i}>{user.username}</li>
-         )
+      ?
+      <div style={{ height: window.innerHeight - 120 + 'px', overflowY: 'auto', wordWrap: 'break-word' }}>
+         <h5 style={{ textAlign: 'center' }}>Users in {roomname}</h5>
+         <ul>
+            {userList.users.map((user, i) => (
+               <li key={i}>{user.username}</li>
+            ))
+            }
+         </ul>
 
-         )
-         
-         
-      }
-      </ul>
-
-        
-      </div> 
-   : null
-
-    
+      </div>
+      : null
+                    
 )
 
 export default UserList;
