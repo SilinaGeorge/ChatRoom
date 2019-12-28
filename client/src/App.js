33b49b4824651cf335268cde3c9189b6a60c56df
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-28 12:18:27 -0500

    notfound path

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import Room from './components/Room/Room'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
     <Router>
       <Route path='/' exact component={Home} />
       <Route path='/room' exact component={Room} />
-      {/* <Route component={NotFound} /> */}
+      <Route component={NotFound} />
     </Router>    
   );
 }
---
+++ client/src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const NotFound =() =>{
+    return(
+        <div>
+            <h>Oops looks like you took a wrong turn!</h>
+        </div>
+    )
+}
+
+export default NotFound
