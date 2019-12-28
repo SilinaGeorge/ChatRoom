7fff28607d9de8335ef34528f65138a1aded8598
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-28 00:05:21 -0500

    Update rootActions.js

--- client/src/actions/rootActions.js
+++ client/src/actions/rootActions.js
@@ -6,13 +6,13 @@ export const SET_ROOMNAME = 'SET_ROOMNAME'
 export function setUsernameAction(username) {
     return {
       type: SET_USERNAME,
-      username
+      payload: username
     }
   }
 
 export function setRoomnameAction(roomname) {
     return {
       type: SET_ROOMNAME,
-      roomname
+      payload: roomname
     }
   }
