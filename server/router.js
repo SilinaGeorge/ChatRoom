e96a3319346d2d836a1b22e5fce47537a8ede46c
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-27 22:11:18 -0500

    Update router.js

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.use(express.static(__dirname, '../client/public'));
+router.use(express.static(path.join(__dirname, '../client/public')));
 
 
 router.get('/', (req,res) => {
