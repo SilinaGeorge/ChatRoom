01f18e5080ba7fdd4261d72efcbc222f91d7ca1d
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-27 21:55:21 -0500

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,17 +2,10 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-express.static(path.join(__dirname, '../client/public'))
-const fs = require('fs')
+
+app.use(express.static(__dirname, '../client/public'));
 
 router.get('/', (req,res) => {
-    fs.access(path.join(__dirname, '../client/public/index.html'), function(err) {
-        if (err && err.code === 'ENOENT') {
-          console.log('not found')
-        }
-        console.log('found')
-      });
-
     res.sendFile("index.html", {root: '../client/public'});
 });
 
