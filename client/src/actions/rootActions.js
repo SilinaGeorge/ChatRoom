2154370bda7eb1aa862e7a72aecd508bfb95e904
Silina George <19698115+SilinaGeorge@users.noreply.github.com>
2019-12-28 00:21:36 -0500

    added actions

---
+++ client/src/actions/rootActions.js
@@ -0,0 +1,18 @@
+
+export const SET_USERNAME = 'SET_USERNAME'
+export const SET_ROOMNAME = 'SET_ROOMNAME'
+
+
+export function setUsernameAction(username) {
+    return {
+      type: SET_USERNAME,
+      payload: {username}
+    }
+  }
+
+export function setRoomnameAction(roomname) {
+    return {
+      type: SET_ROOMNAME,
+      payload: {roomname}
+    }
+  }
